977fa397decf4c9a16b32058392a9c1fd378d04d
Travis Horn <travis@travishorn.com>
2023-06-02 10:07:26 -0500

    All icons added

---
+++ src/lib/20/solid/MinusSmallIcon.svelte
@@ -0,0 +1,9 @@
+<svg
+	xmlns="http://www.w3.org/2000/svg"
+	viewBox="0 0 20 20"
+	fill="currentColor"
+	aria-hidden="true"
+	{...$$props}
+>
+	<path d="M6.75 9.25a.75.75 0 000 1.5h6.5a.75.75 0 000-1.5h-6.5z" />
+</svg>
